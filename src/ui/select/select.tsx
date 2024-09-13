be2991a9901f1e8ca4d77e8b498dbfdbe60269b5
HaJaehyeong <hajae305@gmail.com>
2024-09-13 11:35:08 +0900

    refactoring select component: add comments and optimization

--- src/ui/select/select.tsx
+++ src/ui/select/select.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { RiArrowDownSFill } from '@remixicon/react';
-import React, { Children, ReactElement, useEffect, useState } from 'react';
+import React, { Children, ReactElement, useEffect, useMemo, useState } from 'react';
 import UiMenuItem, { UiMenuItemProps } from '../menu-item/menu-item';
 import UiMenu from '../menu/menu';
 import styles from './select.module.scss';
@@ -22,8 +22,9 @@ type UiSelectProps = {
 /**
  * NOTE(hajae)
  * children을 UiMenuItem Component로 받기 때문에 해당 컴포넌트의 props를 가져오는 처리들이 필요
- * 1. label(children), value를 items state에 저장
- * 2. onClick event를 생성
+ * 1. UiMenuItem으로 Item List를 생성
+ * 2. Default Value prop가 존재하면 Item List에서 찾아서 SET
+ * 3. onClick event를 생성
  */
 const UiSelect: React.FC<UiSelectProps> = ({
   label,
@@ -36,20 +37,29 @@ const UiSelect: React.FC<UiSelectProps> = ({
   helperText,
   onChange,
 }) => {
-  const [item, setItem] = useState<Item>();
+  const [item, setItem] = useState<Item | undefined>(undefined);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  // NOTE(hajae): UiMenuItem의 Props를 이용해서 ItemList를 생성
+  const items = useMemo(() => {
+    const itemList: Item[] = [];
+    Children.forEach(children, (child) => {
+      if (React.isValidElement(child) && child.type === UiMenuItem) {
+        const label = child.props.children as string;
+        const value = child.props.value;
+        itemList.push({ label, value });
+      }
+    });
+    return itemList;
+  }, [children]);
+
+  // NOTE(hajae): defaultValue props가 있을 때 ItemList에서 찾아서 SET
   useEffect(() => {
-    if (defaultValue && children) {
-      Children.forEach(children, (child) => {
-        if (React.isValidElement(child) && child.type === UiMenuItem) {
-          if (child.props.value === defaultValue) {
-            setItem({ label: child.props.children as string, value: defaultValue });
-          }
-        }
-      });
+    if (defaultValue) {
+      const defaultItem = items.find((item) => item.value === defaultValue);
+      if (defaultItem) setItem(defaultItem);
     }
-  }, [defaultValue, children]);
+  }, [defaultValue, items]);
 
   const handleItemClick = (selectedItem: Item) => {
     setItem(selectedItem);
@@ -59,15 +69,19 @@ const UiSelect: React.FC<UiSelectProps> = ({
     }
   };
 
-  const resettingPropsChildrens = Children.map(children, (child) => {
-    if (React.isValidElement(child) && child.type === UiMenuItem) {
-      return React.cloneElement(child as ReactElement<UiMenuItemProps>, {
-        onClick: () =>
-          handleItemClick({ label: child.props.children as string, value: child.props.value as string | number }),
-      });
-    }
-    return child;
-  });
+  // NOTE(hajae): UiMenuItem Components에 onClick Event를 추가
+  const renderChildrenWithOnClickEvent = useMemo(
+    () =>
+      Children.map(children, (child) => {
+        if (React.isValidElement(child) && child.type === UiMenuItem) {
+          return React.cloneElement(child as ReactElement<UiMenuItemProps>, {
+            onClick: () => handleItemClick({ label: child.props.children as string, value: child.props.value }),
+          });
+        }
+        return child;
+      }),
+    [children]
+  );
 
   return (
     <div className={`${styles.selectContainer} ${disabled ? styles.disabled : ''}`}>
@@ -95,7 +109,7 @@ const UiSelect: React.FC<UiSelectProps> = ({
         />
       </fieldset>
       <UiMenu isOpen={isMenuOpen} onClose={() => setIsMenuOpen(!isMenuOpen)}>
-        {resettingPropsChildrens}
+        {renderChildrenWithOnClickEvent}
       </UiMenu>
       {helperText && <div className={`${styles.helperText} ${styles[variant]} ${styles[size]}`}>{helperText}</div>}
     </div>
