0e3797d334e2984ee0ce9510c6fd62cc3d7bedf4
HaJaehyeong <hajae305@gmail.com>
2024-10-02 10:45:03 +0900

    modify checkbox disabled design and add checkbox sample component

--- src/app/(sample)/how-to-use/(ui-components)/checkbox/_components/checkbox-sample/checkbox-sample.tsx
+++ src/app/(sample)/how-to-use/(ui-components)/checkbox/_components/checkbox-sample/checkbox-sample.tsx
@@ -1,4 +1,4 @@
-import UiButton from '@/ui/button/button';
+import UiCheckBox from '@/ui/checkbox/checkbox';
 import { memo } from 'react';
 import styles from './checkbox-sample.module.scss';
 
@@ -7,11 +7,241 @@ const CheckboxSample: React.FC = () => {
     <>
       <div>
         <h4>Variant</h4>
-        <h5>Contained (Default)</h5>
+        <h5>Default</h5>
         <div className={styles.variant}>
           <div className={styles.item}>
-            <UiButton value="Button" />
+            <UiCheckBox />
             <label>Default</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox checked />
+            <label>Checked</label>
+          </div>
+        </div>
+        <h5>With Label</h5>
+        <div className={styles.variant}>
+          <div className={styles.item}>
+            <UiCheckBox label="Label" />
+            <label>Default</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox label="Label" checked />
+            <label>Checked</label>
+          </div>
+        </div>
+
+        <hr />
+
+        <h4>Color</h4>
+        <h5>Primary (Default)</h5>
+        <div className={styles.color}>
+          <div className={styles.item}>
+            <UiCheckBox />
+            <label>Primary</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox label="Label" />
+            <label>With Label</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox checked />
+            <label>Checked</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox label="Label" checked />
+            <label>With Label</label>
+          </div>
+        </div>
+        <h5>Secondary</h5>
+        <div className={styles.color}>
+          <div className={styles.item}>
+            <UiCheckBox color="secondary" />
+            <label>Secondary</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox label="Label" color="secondary" />
+            <label>With Label</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox color="secondary" checked />
+            <label>Checked</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox color="secondary" label="Label" checked />
+            <label>With Label</label>
+          </div>
+        </div>
+        <h5>Info</h5>
+        <div className={styles.color}>
+          <div className={styles.item}>
+            <UiCheckBox color="info" />
+            <label>Info</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox label="Label" color="info" />
+            <label>With Label</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox color="info" checked />
+            <label>Checked</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox color="info" label="Label" checked />
+            <label>With Label</label>
+          </div>
+        </div>
+        <h5>Success</h5>
+        <div className={styles.color}>
+          <div className={styles.item}>
+            <UiCheckBox color="success" />
+            <label>Success</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox label="Label" color="success" />
+            <label>With Label</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox color="success" checked />
+            <label>Checked</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox color="success" label="Label" checked />
+            <label>With Label</label>
+          </div>
+        </div>
+        <h5>Warning</h5>
+        <div className={styles.color}>
+          <div className={styles.item}>
+            <UiCheckBox color="warning" />
+            <label>Warning</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox label="Label" color="warning" />
+            <label>With Label</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox color="warning" checked />
+            <label>Checked</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox color="warning" label="Label" checked />
+            <label>With Label</label>
+          </div>
+        </div>
+        <h5>Error</h5>
+        <div className={styles.color}>
+          <div className={styles.item}>
+            <UiCheckBox color="error" />
+            <label>Error</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox label="Label" color="error" />
+            <label>With Label</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox color="error" checked />
+            <label>Checked</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox color="error" label="Label" checked />
+            <label>With Label</label>
+          </div>
+        </div>
+
+        <hr />
+
+        <h4>Size</h4>
+        <h5>Small</h5>
+        <div className={styles.size}>
+          <div className={styles.item}>
+            <UiCheckBox size="s" />
+            <label>Small</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox label="Label" size="s" checked />
+            <label>Checked</label>
+          </div>
+        </div>
+        <h5>Medium (Default)</h5>
+        <div className={styles.size}>
+          <div className={styles.item}>
+            <UiCheckBox />
+            <label>Medium</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox label="Label" checked />
+            <label>Checked</label>
+          </div>
+        </div>
+        <h5>Large</h5>
+        <div className={styles.size}>
+          <div className={styles.item}>
+            <UiCheckBox size="l" />
+            <label>Large</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox label="Label" size="l" checked />
+            <label>Checked</label>
+          </div>
+        </div>
+
+        <hr />
+
+        <h4>State</h4>
+        <h5>Default</h5>
+        <div className={styles.state}>
+          <div className={styles.item}>
+            <UiCheckBox />
+            <label>Default</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox label="Label" />
+            <label>With Label</label>
+          </div>
+        </div>
+        <h5>Checked</h5>
+        <div className={styles.state}>
+          <div className={styles.item}>
+            <UiCheckBox checked />
+            <label>Default</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox label="Label" checked />
+            <label>With Label</label>
+          </div>
+        </div>
+        <h5>Indeterminate</h5>
+        <div className={styles.state}>
+          <div className={styles.item}>
+            <UiCheckBox checked indeterminate />
+            <label>Default</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox label="Label" checked indeterminate />
+            <label>With Label</label>
+          </div>
+        </div>
+        <h5>Disabled</h5>
+        <div className={styles.state}>
+          <div className={styles.item}>
+            <UiCheckBox disabled />
+            <label>Default</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox label="Label" disabled />
+            <label>With Label</label>
+          </div>
+        </div>
+
+        <h5>Checked Disabled</h5>
+        <div className={styles.state}>
+          <div className={styles.item}>
+            <UiCheckBox checked disabled />
+            <label>Default</label>
+          </div>
+          <div className={styles.item}>
+            <UiCheckBox label="Label" checked disabled />
+            <label>With Label</label>
           </div>
         </div>
       </div>
